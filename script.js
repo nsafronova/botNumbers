027cae855c61ceb67c65e3b43299cce079a74f9e
Natasha Safronova <natasha.safrronova@gmail.com>
2022-04-02 20:54:26 +0300

    bot numbers

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
 'use strict';
 
 const guessGame = () => {
-  alert('Загадывание случайного числа от 1 до 100');
-  let usersNumber = +prompt('Угадай число от 1 до 100');
+  // alert('Загадывание случайного числа от 1 до 100');
+
   let rand = Math.floor(Math.random() * 100) + 1; // возвращает случайное целое число от 1 до 100
   let counter = 10;
+  let usersNumber = prompt('Угадай число от 1 до 100').trim();
 
-  const isNumber = (num) => {
-    return !isNaN(parseFloat(num)) && isFinite(num); //лучшая проверка на число 
+  const isNumber = () => {
+    return !isNaN(parseFloat()) && isFinite(); //лучшая проверка на число 
   };
 
-  const tryCounter = () => {
-    --counter;
-    if (counter == 0) {
-      let conf = confirm('Вы израсходовали все попытки! Ходите сыграть снова? :(');
-      if (conf) {
-        guessGame();
-      } else {
-        alert('Сыграем в другой раз!');
-
-      }
-    }
-  };
-
-  console.log(rand);
+  // console.log(rand);
 
   const round = () => {
-    if (usersNumber == 0) {
+    if (usersNumber === null) {
       alert('Сыграем в другой раз!');
-    } else if (!isNumber(usersNumber)) {
-      usersNumber = +prompt('Это не число! Пожалуйста, введите число');
+    } else if (isNaN(usersNumber) || usersNumber == '') {
+      usersNumber = prompt('Это не число! Пожалуйста, введите число');
       round();
-    } // else if (usersNumber === 0) {
-    //   alert('Сыграем в другой раз!');
-    //}
-    else if (usersNumber > rand && usersNumber != 0) {
-      usersNumber = +prompt('Загаданное число меньше! Осталось ' + counter + ' попыток. Введите другое число');
-      tryCounter();
-      round();
-    } else if (usersNumber < rand && usersNumber != 0) {
-      usersNumber = +prompt('Загаданное число больше! Осталось ' + counter + ' попыток. Введите другое число');
-      tryCounter();
-      round();
-    } else if (usersNumber === rand && usersNumber != 0) {
-      usersNumber = +confirm('Вы угадали число! Поздравляем! В запасе оставалось ' + counter + ' попытки.\nХотите сыграть снова?');
-      if (usersNumber) {
-        guessGame();
-      } else {
-        alert('Сыграем в другой раз!');
-      }
 
+    } else {
+      if (usersNumber === rand) {
+        usersNumber = +confirm('Вы угадали число! Поздравляем! В запасе оставалось ' + counter + ' попытки.\nХотите сыграть снова?');
+        if (usersNumber) {
+          guessGame();
+        } else {
+          alert('Сыграем в другой раз!');
+        }
+      } else if (usersNumber > rand) {
+        counter--;
+        usersNumber = +prompt('Загаданное число меньше! Осталось ' + counter + ' попыток. Введите другое число');
+
+        round();
+      } else if (usersNumber < rand) {
+        counter--;
+        usersNumber = +prompt('Загаданное число больше! Осталось ' + counter + ' попыток. Введите другое число');
+        round();
+      } else if (counter == 0) {
+        counter = confirm('Вы израсходовали все попытки! Ходите сыграть снова?'); {
+          if (counter) {
+            guessGame();
+          } else {
+            alert('Сыграем в другой раз!');
+          }
+        }
+      }
     }
   };
   round();
