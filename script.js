894d0694408a01a525bbc096fdc4219c6e812169
Natasha Safronova <natasha.safrronova@gmail.com>
2022-04-02 15:38:41 +0300

    Бот с рекурсией

--- script.js
+++ script.js
@@ -1,88 +1,48 @@
 'use strict';
 
-let lessNumber;
-// const showAlert = () => {
-//   let win = alert('Вы угадали число!');
-//   let lessNumber = alert('Загаданное число меньше!');
-//   let biggerNumber = alert('Загаданное число больше!');
-//   let notNumber = alert('Введите число!');
-// };
-
-const isNumber = (num) => {
-  return !isNaN(parseFloat(num)) && isFinite(num); //лучшая проверка на число 
-};
-
-const randomInteger = () => {
-
-};
-
 const guessGame = () => {
   alert('Загадывание случайного числа от 1 до 100');
-  let counter = 1;
   let usersNumber = +prompt('Угадай число от 1 до 100');
-  let rand = Math.floor(Math.random() * 100); // возвращает случайное целое число от 0 до 99
-  console.log(typeof rand);
+  let rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1; // возвращает случайное целое число от 1 до 100
+  let counter = 10;
 
+  const isNumber = (num) => {
+    return !isNaN(parseFloat(num)) && isFinite(num); //лучшая проверка на число 
+  };
+
+  const tryCounter = () => {
+    counter--;
+    alert('Осталось ' + counter + ' попыток');
+    if (counter === 0) {
+      confirm('Вы израсходовали все попытки! Ходите сыграть снова? :(');
+      guessGame();
+    } else {
+      return;
+    }
+    return counter;
+  };
+
+  console.log(rand);
 
   const round = () => {
-
-    counter++;
-    if (usersNumber === null) {
+    tryCounter();
+    if (!isNumber(usersNumber)) {
+      usersNumber = +prompt('Это не число! Пожалуйста, введите число');
+      round();
+    } else if (usersNumber === 0) {
       alert('Сыграем в другой раз!');
-    } else if (!isNumber(usersNumber)) {
-      alert('Это не число! Пожалуйста, введите число');
-
     } else if (usersNumber > rand) {
-      alert('Загаданное число меньше!');
-      usersNumber = +prompt('Введите другое число');
+      usersNumber = +prompt('Загаданное число меньше! Введите другое число');
       round();
     } else if (usersNumber < rand) {
-      alert('Загаданное число больше!');
-      usersNumber = +prompt('Введите другое число');
+      usersNumber = +prompt('Загаданное число больше! Введите другое число');
       round();
     } else if (usersNumber === rand) {
-      usersNumber = +alert('Вы угадали число! Тебе понадобилось ' + counter + ' попытки');
+      usersNumber = +confirm('Вы угадали число! Поздравляем! В запасе оставалось ' + counter + ' попытки.\nХотите сыграть снова?');
+      guessGame();
     }
-    // round();
   };
   round();
 };
-// randomInteger(1, 100);
+
 guessGame();
-
-
-
-// function getNumber(rand) {
-//   if (usersNumber === rand) {
-//     alert('Вы угадали!');
-//   } else if (usersNumber > rand) {
-//     alert('Загаданное число меньше!');
-//     usersNumber = prompt('Введите число снова');
-//     return;
-//   } else if (usersNumber < rand) {
-//     alert('Загаданное число больше!');
-//     usersNumber = prompt('Введите число снова');
-//     return;
-//   } else if (!isNumber(usersNumber)) {
-//     alert('Введите число!');
-//     usersNumber = prompt('Введите число снова');
-//     return;
-//   } else {
-//     return;
-//   }
-// }
-
-
-
-// // console.log(randomInteger(1, 100));
-// console.log(getNumber());
-
-
-
-// const getUsersNumber = function () {
-//   do {
-//     usersNumber = +prompt('Сколько будет стоить данная работа?');
-//   } while (!isNumber(usersNumber)); {
-//     return;
-//   }
-// };
